477f4a9d1659ad8a68324e8dcbcc5dd3e12e82f8
Hoa Mai <hoa@ramper.xyz>
2024-09-21 23:13:19 -0700

    Update: Login on the right top corner of the screen.

--- components/header.tsx
+++ components/header.tsx
@@ -20,7 +20,7 @@ async function UserOrLogin() {
   const session = (await auth()) as Session
   return (
     <>
-      {session?.user ? (
+      {/* {session?.user ? (
         <>
           <SidebarMobile>
             <ChatHistory userId={session.user.id} />
@@ -32,9 +32,9 @@ async function UserOrLogin() {
           <IconNextChat className="size-6 mr-2 dark:hidden" inverted />
           <IconNextChat className="hidden size-6 mr-2 dark:block" />
         </Link>
-      )}
+      )} */}
       <div className="flex items-center">
-        <IconSeparator className="size-6 text-muted-foreground/50" />
+        {/* <IconSeparator className="size-6 text-muted-foreground/50" /> */}
         {session?.user ? (
           <UserMenu user={session.user} />
         ) : (
@@ -51,29 +51,12 @@ export function Header() {
   return (
     <header className="sticky top-0 z-50 flex items-center justify-between w-full h-16 px-4 border-b shrink-0 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
       <div className="flex items-center">
+        <h2><b>Free Times</b></h2>
+      </div>
+      <div className="flex items-center justify-end space-x-2">
         <React.Suspense fallback={<div className="flex-1 overflow-auto" />}>
           <UserOrLogin />
         </React.Suspense>
-      </div>
-      <div className="flex items-center justify-end space-x-2">
-        <a
-          target="_blank"
-          href="https://github.com/vercel/nextjs-ai-chatbot/"
-          rel="noopener noreferrer"
-          className={cn(buttonVariants({ variant: 'outline' }))}
-        >
-          <IconGitHub />
-          <span className="hidden ml-2 md:flex">GitHub</span>
-        </a>
-        <a
-          href="https://vercel.com/templates/Next.js/nextjs-ai-chatbot"
-          target="_blank"
-          className={cn(buttonVariants())}
-        >
-          <IconVercel className="mr-2" />
-          <span className="hidden sm:block">Deploy to Vercel</span>
-          <span className="sm:hidden">Deploy</span>
-        </a>
       </div>
     </header>
   )
